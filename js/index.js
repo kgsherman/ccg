90c9f1e025fa21dc9db798ca5e45259763939836
unknown <kesh2051@OIT-ES-L-012.ad.colorado.edu>
2015-03-23 15:19:11 -0600

    fixing iterator

--- js/index.js
+++ js/index.js
@@ -6,10 +6,13 @@ var _ = require('lodash');
 
 var Ship = React.createClass({
 	render: function () {
+		console.log(this.props.data);
+		var name = this.props.data.display;
+		var priceusd = (this.props.data.price && this.props.data.price.usd) ? this.props.data.price.usd : 'Not available';
 		return (
 			<div className="ship">
 				<h1>{this.props.data.display}</h1>
-				${this.props.data.price.usd} | REC {this.props.data.price.rec}
+				${priceusd}
 			</div>
 		);
 	}
@@ -17,14 +20,13 @@ var Ship = React.createClass({
 
 var App = React.createClass({
 	render: function () {
-		var shipNodes = _(this.props.db).values(function (ship) {
-			return (
-				helllo
-			);
-		});
+		var ships = [];
+		for (var ship in db) {
+			ships.push(<Ship data={db[ship]} />);
+		}
 		return (
 			<div className="list">
-				{shipNodes}
+				{ships}
 			</div>
 		);
 	}
